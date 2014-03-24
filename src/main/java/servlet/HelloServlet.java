6211483a053192ae67f711d7bac17d429514ee6e
sjain0@walmartlabs.com <siddhanth.iitb@gmail.com>
2014-03-24 19:49:14 +0530

    Adding sql statement code

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -35,6 +35,7 @@ public class HelloServlet extends HttpServlet {
 			connect = DriverManager
 					.getConnection("jdbc:mysql://us-cdbr-east-05.cleardb.net/heroku_e3f1160c4f489ca?"
 							+ "user=b174f8f64c67e5&" + "password=d2a1f516");
+			statement = connect.createStatement();
 
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
